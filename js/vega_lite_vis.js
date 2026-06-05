d9853fe9a8cbab95a5c8aa83f4e07431918ce225
dbin0017 <dbin0017@student.monash.edu>
2026-06-06 05:26:16 +1000

    Add files via upload

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,24 @@
+var vg_3 = "js/expected_survival_rate.vg.json";
+vegaEmbed("#expected_survival_rate", vg_3).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "js/threatened_status.vg.json";
+vegaEmbed("#threatened_status", vg_4).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "js/cumulative_extinct.vg.json";
+vegaEmbed("#cumulative_extinct", vg_5).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "js/threatened_species_trend.vg.json";
+vegaEmbed("#threatened_species_trend", vg_6).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_7 = "js/threatened_species_by_state.vg.json";
+vegaEmbed("#threatened_species_by_state", vg_6).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
