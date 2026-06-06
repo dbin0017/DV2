590cd3435949cb69a052e760590e20197e7e994a
dbin0017 <dbin0017@student.monash.edu>
2026-06-06 21:48:50 +1000

    Added charts

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,24 +1,54 @@
-var vg_3 = "js/expected_survival_rate.vg.json";
-vegaEmbed("#expected_survival_rate", vg_3).then(function (result) {
+var vg_1 = "js/choropleth_map.vg.json";
+vegaEmbed("#choropleth_map", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_4 = "js/threatened_status.vg.json";
-vegaEmbed("#threatened_status", vg_4).then(function (result) {
+var vg_2 = "js/sunburst_chart.vg.json";
+vegaEmbed("#sunburst_chart", vg_2).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_5 = "js/cumulative_extinct.vg.json";
-vegaEmbed("#cumulative_extinct", vg_5).then(function (result) {
+var vg_3 = "js/extinct_mammals_line_graph.vg.json";
+vegaEmbed("#extinct_mammals_line_graph", vg_3).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_6 = "js/threatened_species_trend.vg.json";
-vegaEmbed("#threatened_species_trend", vg_6).then(function (result) {
+var vg_4 = "js/threatened_species_trend.vg.json";
+vegaEmbed("#threatened_species_trend", vg_4).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_7 = "js/threatened_species_by_state.vg.json";
-vegaEmbed("#threatened_species_by_state", vg_6).then(function (result) {
+var vg_5 = "js/threatened_status.vg.json";
+vegaEmbed("#threatened_status", vg_5).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "js/status_by_class.vg.json";
+vegaEmbed("#status_by_class", vg_6).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_7 = "js/expected_survival_rate.vg.json";
+vegaEmbed("#expected_survival_rate", vg_7).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_8 = "js/threats.vg.json";
+vegaEmbed("#threats", vg_8).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_9 = "js/action_plan.vg.json";
+vegaEmbed("#action_plan", vg_9).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_10 = "js/dot_map.vg.json";
+vegaEmbed("#dot_map", vg_10).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_11 = "js/marine_protected_areas.vg.json";
+vegaEmbed("#marine_protected_areas", vg_11).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
